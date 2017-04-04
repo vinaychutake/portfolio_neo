b1c35eda4e4c76e4fdd7133b5ad876f66c126f9c
vinay <vinay.chutake@neosofttech.com>
2017-04-04 18:21:06 +0530

    google pie chart small change

--- portfolio/static/portfolio/js/canvas_chart.js
+++ portfolio/static/portfolio/js/canvas_chart.js
@@ -1,32 +1,5 @@
 
 
-/*function chartload(data)
-    {
-    var canvas = document.getElementById("can");
-    var ctx = canvas.getContext("2d");
-    var lastend = 0;
-    var data = data; // If you add more data values make sure you add more colors
-    var myTotal = 0; // Automatically calculated so don't touch
-    var myColor = ['red', 'green', 'blue','black']; // Colors of each slice
-    var name = ['a','b','c'];
-
-    for (var e = 0; e < data.length; e++) {
-      myTotal += data[e];
-    }
-
-    for (var i = 0; i < data.length; i++) {
-      ctx.fillStyle = myColor[i];
-      ctx.beginPath();
-      ctx.moveTo(canvas.width / 2, canvas.height / 2);
-      // Arc Parameters: x, y, radius, startingAngle (radians), endingAngle (radians), antiClockwise (boolean)
-      ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, lastend, lastend + (Math.PI * 2 * (data[i] / myTotal)), false);
-      ctx.lineTo(canvas.width / 2, canvas.height / 2);
-      ctx.fill();
-      lastend += Math.PI * 2 * (data[i] / myTotal);
-    }
-    }
-*/
-
 function wordcloudload(){
         try {
           TagCanvas.Start('myCanvas','tags',{
